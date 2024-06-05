3cca6f9ebb92ef51dbeb024477812e6acfb2f27c
bocurry <562371290@qq.com>
2024-06-05 23:56:36 +0900

    add chat route,pass postman local test

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
-from flask import Flask, redirect, url_for, request, render_template, session
+from flask import Flask, redirect, url_for, request, render_template, session, jsonify
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()  # take environment variables from .env.
+os.environ['http_proxy'] = "http://localhost:7890"
+client = OpenAI(
+    # api_key =os.environ.get("OPENAI_API_KEY"),
+    api_key = 'sk-1TX8AEx5RsmDF9vpoN19jYZ394drHS7ecgI1npSoWycsLjxK',
+    base_url = "https://api.moonshot.cn/v1",
+)
 
 app = Flask(__name__)
-
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    user_message = data.get("message")
+
+    response = client.chat.completions.create(
+        model="moonshot-v1-8k",
+        messages = [
+            {"role": "system", "content": "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，不可翻译成其他语言。"},
+            {"role": "user", "content": user_message}
+        ],
+        max_tokens=150,
+        temperature = 0.3,
+    )
+    print(response.choices[0].message.content)
+
+    # 将整个响应返回给客户端
+    response_dict = response.to_dict()
+    return jsonify(response_dict)
+
+if __name__ == '__main__':
+    app.run(debug=True)
